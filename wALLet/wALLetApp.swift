0c42a6f2409d4f2a9f024b71d12f4c2de5d679a8
Gio Peña <thegiologist@gmail.com>
2023-01-02 21:20:16 -0600

    Initial Commit

---
+++ wALLet/wALLetApp.swift
@@ -0,0 +1,17 @@
+//
+//  wALLetApp.swift
+//  wALLet
+//
+//  Created by Gio Peña on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct wALLetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
