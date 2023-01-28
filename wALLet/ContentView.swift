c6d934066259597e4a74ecf214814e5ae20e9d86
Gio Peña <thegiologist@gmail.com>
2023-01-27 21:03:13 -0600

    Setting up structure.

--- wALLet/ContentView.swift
+++ wALLet/ContentView.swift
@@ -6,21 +6,66 @@
 //
 
 import SwiftUI
+import PassKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Gio!")
+        NavigationStack {
+            List {
+                NavigationLink {
+                    PassView()
+                } label: {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, Gio!")
+                }
+                NavigationLink {
+                    Text("Barcode Scanner")
+                } label: {
+                    Image(systemName: "barcode")
+                    Text("Barcode Scanner")
+                }
+
+            }
         }
-        .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+
+
+
+
+struct PassView : View {
+    var body: some View {
+        VStack {
+            Button {
+                addPass()
+            } label: {
+                Text("Add Pass")
+            }
+
+        }
+        .onAppear{ setup() }
+    }
+    
+    
+    
+    private func setup() {
+        
+    }
+    
+    
+    private func addPass() {
+        print(#function)
     }
 }
